7f93a984810e0cf632f6e694145b86f54e45c848
lyml <1074325187@qq.com>
2021-03-22 02:46:43 +0800

    test8

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ module.exports = {
   },
   hooks: {
     'page:before': function(page) {
+      if(this.options.pluginsConfig['add-js']) {
+        var _urls = this.options.pluginsConfig['tbfed-pagefooter']['urls'] || [];
+        var _html = '';
+        for(var i = 0; i < _urls.length; i++){
+          if(_urls[i]){
+            _html += '<script type="text/javascript" src="'+_urls[i]+'"></script>'
+          }
+        }
+        page.content = _html + page.content;
+      }
       return page;
     }
   }
