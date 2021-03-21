61574d615a11b15e025381ea047a5c4ecc3ca262
lyml <1074325187@qq.com>
2021-03-22 02:15:45 +0800

    test1

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 var moment = require('moment');
 module.exports = {
   book: {
-    assets: './assets',
-    css: [
-      'footer.css'
-    ],
+    assets: './assets'
   },
   hooks: {
     'page:before': function(page) {
-      var _label = 'File Modify: ',
-          _format = 'YYYY-MM-DD HH:mm:ss',
-          _copy = 'powered by Gitbook'
-      if(this.options.pluginsConfig['tbfed-pagefooter']) {
-        _label = this.options.pluginsConfig['tbfed-pagefooter']['modify_label'] || _label;
-        _format = this.options.pluginsConfig['tbfed-pagefooter']['modify_format'] || _format;
-
-        var _c = this.options.pluginsConfig['tbfed-pagefooter']['copyright'];
-        _copy = _c ? _c + ' all right reserved，' + _copy : _copy;
+      if(this.options.pluginsConfig['add-js']) {
+        var _urls = this.options.pluginsConfig['tbfed-pagefooter']['urls'] || [];
+        var _html = '';
+        for(var i in urls){
+          if(urls[i]){
+            html += '<script type="text/javascript" src="'+urls[i]+'"></script>'
+          }
+        }
       }
-      var _copy = '<span class="copyright">'+_copy+'</span>'
-      var str = ' \n\n<footer class="page-footer">' + _copy +
-        '<span class="footer-modification">' +
-        _label +
-        '\n{{file.mtime | date("' + _format +
-        '")}}\n</span></footer>'
-      page.content = page.content + str;
+      page.content = _html + page.content;
       return page;
-    }
-  },
-  filters: {
-    date: function(d, format) {
-      return moment(d).format(format)
     }
   }
 };
